e42642ba76ebe54367345b36fd617fcddabf5d37
FatimaZohraaa <fati.zahra2018@gmail.com>
2022-11-13 19:08:46 +0100

    adding routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'tour-of-heroes';
+
+  constructor(private router: Router) {}
+  dashboardNavigate() {
+    this.router.navigate(['/']);
+  }
+
+  heroesListNavigate() {
+    this.router.navigate(['/heroes']);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroesListComponent } from './heroes-list/heroes-list.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { DetailsComponent } from './details/details.component';
+
+import { Routes } from '@angular/router';
+import { RouterModule } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'heroes', component: HeroesListComponent },
+  { path: 'heroes/hero/:id', component: DetailsComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesListComponent,
+    DashboardComponent,
+    DetailsComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(appRoutes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
