d608e1ba668dbc926ded86fd0837630e19baceab
FatimaZohraaa <fati.zahra2018@gmail.com>
2022-11-25 21:59:54 +0100

    added heroesList as a subject to the service

--- src/app/services/heroesData/heroes-data.service.ts
+++ src/app/services/heroesData/heroes-data.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { startWith, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -20,26 +21,33 @@ export class HeroesDataService {
     return this.heroesList;
   }
 
+  heroeslistSubject = new Subject<{ name: string; id: string }[]>();
+
   /**
    * Adds a new hero to the list of heroes
    * @param {object} hero  - object containing the new hero details
    */
+
   addHero(hero: { name: string; id: string }) {
     this.heroesList = [...this.heroesList, hero];
+    this.heroeslistSubject.next(this.heroesList);
   }
 
   /**
    * Deletes hero with selected id from the list of heroes
    * @param {string} id - id of the hero to delete
    */
+
   deleteHero(id: string) {
     this.heroesList = this.heroesList.filter((hero) => hero.id !== id);
+    this.heroeslistSubject.next(this.heroesList);
   }
 
   /**
    * Updates the details of the hero in the list of heroes
    * @param {object} editedHero - object containing updated details of the edited hero
    */
+
   editHero(editedHero: { name: string; id: string }) {
     this.heroesList.map((hero) => {
       if (hero.id == editedHero.id) {
